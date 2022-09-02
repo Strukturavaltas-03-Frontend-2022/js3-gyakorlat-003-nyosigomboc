c86bb669d4e102e772c53b2483285e50501f447d
Gyozo Miklos <nyosdroid@gmail.com>
2022-09-02 02:36:48 +0200

    done

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,1 +1,22 @@
+const cookieHandler = {
+  getAll() {
+    return document.cookie.split(';').reduce((prev, curr) => {
+      const [, key, value] = /^[ ]*([^=]+)=([^;]*)/.exec(curr);
+      const newObj = prev;
+      newObj[key] = value;
+      return newObj;
+    }, {});
+  },
+  toSessionStorage() {
+    Object.entries(this.getAll()).forEach(([key, value]) => {
+      sessionStorage.setItem(key, value);
+    });
+  },
+  flush() {
+    Object.keys(this.getAll()).forEach((key) => {
+      document.cookie = `${key}=; expires=${new Date(0).toUTCString()}`;
+    });
+  },
+};
 
+export { cookieHandler };
